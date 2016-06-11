e9291813287db331cd635c556b9368f5b0a525ab
silvercobraa <silver.cobra1n@gmail.com>
2016-06-11 18:11:45 -0400

    Se eliminó el #include "Traversals.h"

--- main.cpp
+++ main.cpp
@@ -46,8 +46,13 @@ int main(int argc, char const *argv[])
 
 	Graph<Edge*>* g = new Graph<Edge*>(10);
 	g->print();
-	Edge* e = new Edge(1, 3);
+	Edge* e = new Edge(4, 3);
 	g->add_edge(e, 4, 5);
+	g->add_edge(e, 2, 1);
+	g->add_edge(e, 2, 4);
+	g->add_edge(e, 5, 2);
+	g->add_edge(e, 8, 7);
+	g->add_edge(e, 2, 2);
 	g->print();
 	std::vector<int> v = g->get_neighbours(5);
 	std::cout << "size v:" << v.size() << std::endl;
@@ -56,7 +61,11 @@ int main(int argc, char const *argv[])
 	std::cout << "v[" << i << "]: " << v[i] << std::endl;
 	}
 
-
+	Graph<Edge*> g2 = g->clone();
+	g2.print();
+	std::vector<int> connected_component_4 = BFS(&g2, 4);
+	Graph<Edge*> g3 = g2.get_connected_component(4);
+	g3.print();
 	// CASO DE PRUEBA CON PUNTEROS A OBJETOS DE TIPO PAIR<INT, FLOAT>
 	//Graph<std::pair<int, float>*>* g = new Graph<std::pair<int, float>*>(5);
 	//g->print();
